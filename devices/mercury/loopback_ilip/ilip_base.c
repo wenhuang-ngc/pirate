32e188b4f676a71de963d254d48d185fa71f3f74
Michael Spiegel <michael.spiegel@twosixlabs.com>
2020-03-12 08:25:54 -0400

    Fallback when ktime_get_boottime_ns is not available

--- devices/mercury/loopback_ilip/ilip_base.c
+++ devices/mercury/loopback_ilip/ilip_base.c
@@ -1030,10 +1030,14 @@ static void gaps_ilip_copy( struct work_struct *work )
     }
 
     /* device time we start processing */
-    #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)
+    #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,3,0)
     /* Ubuntu 19.10 API change */
     t = ktime_get_boottime_ns();
-    #else 
+    #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,0)
+    struct timespec64 ts64;
+    ktime_get_boottime_ts64(&ts64);
+    t = ts64.tv_sec * 1000000000 + ts64.tv_nsec;
+    #else
     /* RedHat 7.x API */
     t = ktime_get_boot_ns();
     #endif
@@ -1215,10 +1219,13 @@ static void gaps_ilip_copy( struct work_struct *work )
         }
         memcpy(cp->dst_data, cp->src_data, cp->dst->block_size );
         /* Record delta ILIP time in the receive buffer */
-        #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)
+        #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,3,0)
         /* Ubuntu 19.10 */
         msg->time.ilip_time = ktime_get_boottime_ns() - msg->time.ilip_time ;
-        #else 
+        #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,0)
+        ktime_get_boottime_ts64(&ts64);
+        msg->time.ilip_time = ts64.tv_sec * 1000000000 + ts64.tv_nsec - msg->time.ilip_time ;
+        #else
         /* RedHat 7.x */
         msg->time.ilip_time = ktime_get_boot_ns() - msg->time.ilip_time ;
         #endif
